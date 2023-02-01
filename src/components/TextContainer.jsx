6bc1aac1897c075b83081cb76af19a97734c8709
Ankit Jha <jhaa11701@gmail.com>
2023-02-01 14:20:09 +0530

    minor change

--- src/components/TextContainer.jsx
+++ src/components/TextContainer.jsx
@@ -5,15 +5,12 @@ const TextContainer = ({isPreview}) => {
   const data = ''
   const text = isPreview ? 'PREVIEW' : 'MARKDOWN';
   return (
-    <>
-        <div className='w-1/2  bg-[#151619] text-white h-full '>
+        <div className={`w-1/2  bg-[#151619] text-white h-full ${isPreview ? '' : 'mr-0.5'}`}>
     <Heading text={text}/>
     <div className='w-full flex'>
     <textarea className='bg-transparent w-full h-screen  outline-none p-4 resize-none overflow-hidden' disabled ={isPreview}/>
     </div>
     </div>
-   {isPreview ? '':<span className='h-screen bg-gray-600 w-0.5'></span>} 
-    </>
   )
 }
 
