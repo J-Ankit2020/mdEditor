d7d2684f89fe51978756ed4e8ab0c930ac0b0179
ishar <isharjain19@gmail.com>
2023-02-01 23:41:47 +0530

    feat: save feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {ImBin} from 'react-icons/im'
 import {FiDownload} from 'react-icons/fi'
 import {AiOutlineSave} from 'react-icons/ai'
-const Header = ({downloadHandler}) => {
+const Header = ({downloadHandler, saveHandler}) => {
   return (
     <div className='flex justify-between  bg-[#343A40] text-white items-center py-2'>
         <h1 className='left tracking-widest font-sans font-semibold ml-3 text-xl'>MARKDOWN</h1>
@@ -11,9 +11,10 @@ const Header = ({downloadHandler}) => {
           <button className='bg-orange-600  rounded-md hover:bg-orange-400 flex justify-between items-center p-2' onClick={downloadHandler}>
             <span className='mr-1'><FiDownload/></span>
           Download</button>
-          <button className='bg-orange-600  rounded-md hover:bg-orange-400 flex justify-between items-center p-2'>
+          <button className='bg-orange-600  rounded-md hover:bg-orange-400 flex justify-between items-center p-2' onClick={saveHandler}>
             <span className='mr-1'><AiOutlineSave/></span>
-          Save Changes</button>
+          Save Changes
+          </button>
         </div>
     </div>
   )
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Heading from './Heading'
 import '../Preview.css' 
 const Preview = ({text}) => {
@@ -6,7 +6,7 @@ const Preview = ({text}) => {
    <div className = "w-1/2  bg-[#151619] text-white h-full">
     <Heading text='PREVIEW'/>
     <div className='w-full ml-1'>
-    <div name="markdown-box" className='bg-transparent w-full h-screen p-4 overflow-hidden' dangerouslySetInnerHTML={{'__html': text}}></div>
+    <div name="markdown-box" className='bg-transparent w-full h-screen p-4 overflow-hidden' dangerouslySetInnerHTML={{'__html':text}} ></div>
     </div>
     </div>
   )
