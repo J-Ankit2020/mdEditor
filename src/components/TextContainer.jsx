4e3d4779820060ddfabeb7f9ff43500aa0c53dc3
Ankit Jha <jhaa11701@gmail.com>
2023-02-01 13:43:42 +0530

    added boundary to text box

--- src/components/TextContainer.jsx
+++ src/components/TextContainer.jsx
@@ -5,10 +5,15 @@ const TextContainer = ({isPreview}) => {
   const data = ''
   const text = isPreview ? 'PREVIEW' : 'MARKDOWN';
   return (
+    <>
         <div className='w-1/2  bg-[#151619] text-white h-full '>
     <Heading text={text}/>
-    <textarea className='bg-transparent w-full  outline-none p-4 h-screen resize-none ' disabled ={isPreview}/>
+    <div className='w-full flex'>
+    <textarea className='bg-transparent w-full h-screen  outline-none p-4 resize-none overflow-hidden' disabled ={isPreview}/>
     </div>
+    </div>
+   {isPreview ? '':<span className='h-screen bg-gray-600 w-0.5'></span>} 
+    </>
   )
 }
 
