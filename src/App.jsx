e589bea6894328c843c21d63c8d4fc7b7a57d5f2
ishar <isharjain19@gmail.com>
2023-02-02 23:45:46 +0530

    feat:added delete feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {marked} from 'marked';
 import Header from './components/Header'
 import Markdown from './components/Markdown'
 import Preview from './components/Preview'
 
 function App() {
-    const [html, setHtml] = useState("");
-    const [rawMD, setRawMD] = useState('')
+    const [html, setHtml] = useState(" ");
+    const [rawMD, setRawMD] = useState(" ")
     const converter = (markdown) => {
       setHtml(marked(markdown));
     }
@@ -24,14 +24,32 @@ function App() {
         window.localStorage.setItem("rawMD",rawMD)
         window.alert("Your progress has been saved")
     }
-
+    const deleteHander=()=>{
+        setRawMD('')
+        converter('')
+        window.localStorage.removeItem("rawMD")
+    }
+    useEffect(()=>{
+    console.log("here")
+    const rawMD = window.localStorage.getItem("rawMD")
+    if(rawMD!=null)
+    { 
+        converter(rawMD)
+        setRawMD(rawMD)
+    }
+    else {
+        converter('')
+        setRawMD('')
+    }
+  },[])
     return (
         <div className="App">
-        <Header downloadHandler={downloadHandler} saveHandler={saveHandler}/>
-        <div className='flex'>
-        <Markdown converter={converter} setMD={setRawMD}/>
-        <Preview text = {html}/>
-        </div>
+            <Header downloadHandler={downloadHandler} saveHandler={saveHandler} deleteHandler={deleteHander}
+            />
+            <div className='flex'>
+            <Markdown md={rawMD}  converter={converter} setMD={setRawMD}/>
+            <Preview text = {html}/>
+            </div>
         </div>
     )
 }
