ab87ee8c5fe90046241f57717ff9121d0e6cefd6
ishar <isharjain19@gmail.com>
2023-02-03 19:47:43 +0530

    added tailwind

--- markdown-editor/src/App.jsx
+++ markdown-editor/src/App.jsx
@@ -2,8 +2,8 @@
 
 function App() {
   return (
-    <div className="">
-     Welcome to Solution challenge 101
+    <div className="font-bold flex justify-center items-center h-[100vh] text-4xl bg-gradient-to-r from-rose-400 to-red-500 ">
+     <div className="animate-bounce">Welcome to Solution challenge 101</div>
     </div>
   )
 }
